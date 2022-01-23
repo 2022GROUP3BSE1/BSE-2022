ccd547282e5da4245816fabe2b42a24ced1e9654
2022GROUP3BSE1 <apastro34@gmail.com>
2022-01-23 03:07:50 -0800

    Assignment 2

--- src/chapter2/exercise6.py
+++ src/chapter2/exercise6.py
@@ -1,6 +1,8 @@
-x_1 = float(input('enter first point'))
-x_2 = float(input('enter second point'))
-y_1 = float(input('enter value of y1'))
-y_2 = float(input('enter value of y2'))
+x_1 = float(input('Enter value of x1: '))
+y_1 = float(input('Enter value of y1: '))
+x_2 = float(input('Enter value of x2: '))
+y_2 = float(input('Enter value of y2: '))
+
 distance = ((x_2-x_1)**2+(y_2-y_1)**2)**0.5
-print(distance)
+
+print('>>>Distance between the two points = ',distance,'units')
