de9ce0ecf64f4c235bd13620bb39ccbbe696d2d6
GROUP13BSE12022 <apastro34@gmail.com>
2022-02-01 02:29:44 -0800

    Assignment 3

--- src/chapter2/exercise8.py
+++ src/chapter2/exercise8.py
@@ -1,7 +1,7 @@
-c = int(input('initial amount of investment '))
+c = int(input('initial amount of investment: '))
 r = float(input('yearly rate or interest '))
 t = float(input('number of years until maturation '))
 n = float(input(' number of times the interest is compounded per year '))
 x = t*n
-p = c*(1*r/n)**(x)
-print('p:', p)
+p = round(c*(1+r/n)**x, 2)
+print('p', p)
