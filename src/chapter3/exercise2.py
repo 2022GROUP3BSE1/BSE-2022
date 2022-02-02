f9b1120c5333b095ca5502182a2644fa691ba2fb
2022GROUP3BSE1 <apastro34@gmail.com>
2022-02-02 00:34:20 -0800

    Assignment 3: Exercises 1 and 2

---
+++ src/chapter3/exercise2.py
@@ -0,0 +1,18 @@
+try:
+    Hours = input('Enter hours: ')
+    Hours = float(Hours)
+
+    Rate = input('Enter rate: ')
+    Rate = float(Rate)
+
+    if Hours>40:
+        extra_hours=Hours-40
+        extra_pay=extra_hours*1.5*Rate
+        Pay=(40*Rate)+extra_pay
+    else:
+        Pay=Hours*Rate
+
+    print('Pay: ' ,Pay)
+
+except:
+    print('Error, please enter numeric input')
