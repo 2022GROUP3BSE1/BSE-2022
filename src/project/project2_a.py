22032d3388d73164beb5e65cf0f823c1590ab183
2022GROUP3BSE1 <apastro34@gmail.com>
2022-03-13 15:46:28 +0300

    Added Try and Except to catch error if Measles file is not found and also added all for user to copy all the data

--- src/project/project2_a.py
+++ src/project/project2_a.py
@@ -1,6 +1,9 @@
 # PROGRAM THAT COPIES DATA FROM MEASLES FILE FOR A GIVEN YEAR TO A FILE SELECTED BY A USER
 
-measles_file = open("measles.txt", "r")
+try:        # TRY AND EXCEPT TO CHECK IF MEASLES FILE IS IN DIRECTORY
+    measles_file = open("measles.txt", "r")
+except:
+    print("Measles file not found in current directory!")
 
 output_file = input("Enter name of file: ")     # PROMPTING USER FOR FILE NAME TO COPY DATA TO
 file_handle = open(output_file, "w")        # OPENING THE USER'S FILE AS WRITABLE
@@ -8,10 +11,13 @@ file_handle = open(output_file, "w")        # OPENING THE USER'S FILE AS WRITABL
 year = input("Enter year: ")        # PROMPTING USER FOR YEAR TO COPY DATA FROM
 
 for line in measles_file:       # READING LINES FROM MEASLES FILE
-    year_field = line[84:]       # STARTING INDEX FOR YEAR FIELD IS 84
-    year_field = year_field.strip()       # STRIPPING TO REMOVE WHITE SPACES
-    if year_field.startswith(year):
-        file_handle.write(line)     # WRITING ALL LINES THAT HAVE THE INPUT YEAR IN THE OUTPUT FILE
+    if year.lower() == "" or year == "all":
+        file_handle.write(line)
+    else:
+        year_field = line[84:]       # STARTING INDEX FOR YEAR FIELD IS 84
+        year_field = year_field.strip()       # STRIPPING TO REMOVE WHITE SPACES
+        if year_field.startswith(year):
+            file_handle.write(line)     # WRITING ALL LINES THAT HAVE THE INPUT YEAR IN THE OUTPUT FILE
 
 file_handle.close()
 
