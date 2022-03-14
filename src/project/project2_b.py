96b078daed808e2040b768cca3266f997d72f456
2022GROUP3BSE1 <apastro34@gmail.com>
2022-03-14 21:08:04 +0300

    Fixed output for project2_b to display count and average

--- src/project/project2_b.py
+++ src/project/project2_b.py
@@ -13,6 +13,8 @@ def open_file():
 
 # DEFINING A FUNCTION THAT WILL PROCESS THE FILE OBJECT FROM THE OPEN FILE FUNCTION
 def process_file(file_object):
+    count = 0       # INITIATING COUNT FOR RECORDS AS 0
+    total_percent = 0
     year = input("Enter year: ")
 
     # PRINTING A MENU FOR ENTRY VALUES FOR INCOME LEVEL
@@ -23,25 +25,21 @@ def process_file(file_object):
             4-High income''')
     
     while True:     # WHILE LOOP TO KEEP PROMPTING UNTIL CORRECT INPUT FOR INCOME LEVEL IS ENTERED
-        try:
-            income_level = input("Enter income level: ")
+        income_level = input("Enter income level: ")
 
-            if income_level == '1':
-                income_level = 'WB_LI'
-                break
-            elif income_level == '2':
-                income_level = 'WB_LMI'
-                break
-            elif income_level == '3':
-                income_level == 'WB_UMI'
-                break
-            elif income_level == '4':
-                income_level = 'WB_HI'
-                break
-            else:
-                raise ValueError        # RAISING AN ERROR FOR ANY INPUT WHICH DOES NOT MATCH MENU OF ENTRIES
-
-        except:     # CATCHING AN ERROR IF INPUT DOES NOT MATCH MENU OF ENTRIES
+        if income_level == '1':
+            income_level = 'WB_LI'
+            break
+        elif income_level == '2':
+            income_level = 'WB_LMI'
+            break
+        elif income_level == '3':
+            income_level = 'WB_UMI'
+            break
+        elif income_level == '4':
+            income_level = 'WB_HI'
+            break
+        else:
             print("Invalid input!")
 
     for line in file_object:        # READING INDIVIDUAL LINES IN THE FILE
@@ -49,14 +47,20 @@ def process_file(file_object):
         # READING THE FIELDS OF YEAR AND INCOME LEVEL
         year_field = line[88:]
         income_field = line[50:58]
+        percentage_field = line[59:62]
 
         # STRIPING TO REMOVE WHITE SPACES
         year_field = year_field.strip()
         income_field = income_field.strip()
+        percentage_field = percentage_field.strip()
         
         # PRINTING LINES WITH CORRESPONDING YEARS AND INCOME LEVELS
-        if year_field == year and income_field == income_level:
-            print(line)
+        if year_field.startswith(year) and income_field == income_level:
+            count = count + 1
+            total_percent = total_percent + int(percentage_field)
+            
+    print(f"\n>>>Count for records: {count}")
+    print(f">>>Average percent: {round((total_percent/count),1)}")
 
 
 # DEFINING MAIN FUNCTION TO CALL THE open_file FUNCTION AND process_file FUNCTION
